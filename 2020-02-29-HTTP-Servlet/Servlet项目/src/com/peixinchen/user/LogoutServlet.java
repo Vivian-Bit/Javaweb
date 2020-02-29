ba100a3271e72a23cb3bb9cebc0794dddd60953a
Vivian-Bit <2075864155@qq.com>
2020-02-29 23:02:20 +0800

    Cookie_Session

---
+++ 2020-02-29-HTTP-Servlet/Servlet项目/src/com/peixinchen/user/LogoutServlet.java
@@ -0,0 +1,4 @@
+package com.peixinchen.user;
+
+public class LogoutServlet {
+}
---
+++ 2020-02-29-HTTP-Servlet/Servlet项目/src/com/peixinchen/user/User.java
@@ -0,0 +1,7 @@
+package com.peixinchen.user;
+
+public class User {
+    int id;
+    String username;
+    String nickname;
+}
